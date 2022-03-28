f829d2292ed1334f6ae73d13dc5c6a3c9f185c54
yasikrehman <yasikrehman007@gmail.com>
2022-03-28 17:17:11 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./style.css";
+import React, { useReducer, useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function stateUpdate(state, action) {
+    switch (action.type) {
+      case 'increment':
+        return setCount((count) => count + 1);
+      case 'decrement':
+        return setCount((count) => count - 1);
+    }
+  }
+
+  const [state, dispatch] = useReducer(stateUpdate, count);
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>count:{count}</h1>
+      <p onClick={() => dispatch({ type: 'increment' })}>increment</p>
     </div>
   );
 }
